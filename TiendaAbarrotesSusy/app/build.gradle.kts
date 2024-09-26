57cb0953cf904be1f068d2ea4e7974e601820c24
EquipoUce <equipouce5@gmail.com>
2024-09-26 16:09:25 -0500

    seccion de compra

--- TiendaAbarrotesSusy/app/build.gradle.kts
+++ TiendaAbarrotesSusy/app/build.gradle.kts
@@ -52,5 +52,5 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-database")
 }
